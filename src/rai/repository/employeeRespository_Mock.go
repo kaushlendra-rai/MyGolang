f18e6c7ce198aad2952882bfff086316e80882ac
Kaushlendra Rai <rai.kaushal@gmail.com>
2019-04-22 14:38:19 +0530

    Added simple Testify changes

--- src/rai/repository/employeeRespository_Mock.go
+++ src/rai/repository/employeeRespository_Mock.go
@@ -4,3 +4,8 @@ import ()
 
 // https://github.com/DATA-DOG/go-sqlmock
 // go get github.com/DATA-DOG/go-sqlmock
+
+// OR Use testify for mocks
+
+// go get github.com/stretchr/testify
+// https://github.com/stretchr/testify
